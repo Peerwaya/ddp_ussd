3b8c17054518a85e6cad6a7c4bd5a2c6b8fa7fde
Onyemaechi Okafor <evenmatrix@gmail.com>
2015-09-03 09:00:31 +0100

    added gsm-modem to send ussd commands

--- ddp-ussd.js
+++ ddp-ussd.js
@@ -1,4 +1,27 @@
-
+var commander = require('commander');
 var proxy = require('./lib/proxy');
+var optionsParser = commander
+  .version(require('./package.json').version)
+  .option('-m, --meteorPort <port>', 'Meteor App Port [default: 3000]', 3000)
+  .option('-p, --proxyPort <port>', 'DDP Proxy Port [default: 3030]', 3030)
+  .option('-u, --ussdUrl <ussd>', 'USSD URL [default: xxx]', "")
+  .option('-v, --version', 'print version', false);
 
-proxy(3000, 3030,"");
+var argv = optionsParser.parse(process.argv);
+proxy(argv.meteorPort, argv.proxyPort, argv.ussdUrl);
+
+
+var serialport = require('serialport');
+var SerialPort = serialport.SerialPort;
+
+// list serial ports:
+serialport.list(function (err, ports) {
+  ports.forEach(function(port) {
+    console.log(port.comName);
+  });
+});
+
+
+
+
+
